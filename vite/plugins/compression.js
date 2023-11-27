16affced71d6c01e73b1641573f48c9db778b909
coderlmw <2383250163@qq.com>
2023-11-27 10:01:38 +0800

    修改打包配置

--- vite/plugins/compression.js
+++ vite/plugins/compression.js
@@ -8,7 +8,11 @@ export default function createCompression(env) {
     if (compressList.includes('gzip')) {
       plugin.push(
         compression({
+          // 生成文件后缀
           ext: '.gz',
+          // 体积大于threshold则进行压缩，单位为b
+          threshold: 1024 * 10,
+          // 压缩后是否删除源文件
           deleteOriginFile: false,
         })
       )
@@ -16,8 +20,11 @@ export default function createCompression(env) {
     if (compressList.includes('brotli')) {
       plugin.push(
         compression({
+          // 生成文件后缀
           ext: '.br',
+          // 压缩算法
           algorithm: 'brotliCompress',
+          // 压缩后是否删除源文件
           deleteOriginFile: false,
         })
       )
--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -1,14 +1,12 @@
 import vue from '@vitejs/plugin-vue'
-
+import setupExtend from 'unplugin-vue-setup-extend-plus/vite'
 import createAutoImport, { createComponents } from './auto-import'
 import createSvgIcon from './svg-icon'
 import createCompression from './compression'
-import createSetupExtend from './setup-extend'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
-  const vitePlugins = [vue()]
+  const vitePlugins = [vue(), setupExtend({})]
   vitePlugins.push(createAutoImport())
-  vitePlugins.push(createSetupExtend())
   vitePlugins.push(createComponents())
   vitePlugins.push(createSvgIcon(isBuild))
   isBuild && vitePlugins.push(...createCompression(viteEnv))
