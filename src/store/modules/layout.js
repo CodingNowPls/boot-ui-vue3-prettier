4ff56010530835636ea40a5e79d1e6beaf092b3a
coderlmw <lmwl0113@163.com>
2023-11-28 23:40:12 +0800

    修改readMe,修改动画默认值

--- src/store/modules/layout.js
+++ src/store/modules/layout.js
@@ -11,7 +11,7 @@ export const useConfig = defineStore(
       // 后台布局方式，可选值<Default|Classic|Streamline|Double>
       layoutMode: 'Default',
       // 后台主页面切换动画，可选值<slide-right|slide-left|el-fade-in-linear|el-fade-in|el-zoom-in-center|el-zoom-in-top|el-zoom-in-bottom>
-      mainAnimation: 'slide-right',
+      mainAnimation: 'el-fade-in',
       // 是否暗黑模式
       isDark: false,
 
