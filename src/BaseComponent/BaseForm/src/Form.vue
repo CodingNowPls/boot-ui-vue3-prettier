7216f783d282ac859463a7fbaf28e38fa6d9f75d
coderlmw <2383250163@qq.com>
2024-07-23 11:08:36 +0800

    修改样式

--- src/BaseComponent/BaseForm/src/Form.vue
+++ src/BaseComponent/BaseForm/src/Form.vue
@@ -253,6 +253,8 @@ defineExpose({
     margin: 0px !important;
     font-weight: 500;
     color: var(--el-text-color-primary);
+    display: flex;
+    align-items: center;
   }
   :deep(.el-cascader) {
     width: 100%;
