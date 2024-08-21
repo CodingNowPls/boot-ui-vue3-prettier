7b12344c7e173b4c5c1717f9565a1f3ec8066934
coderlmw <2383250163@qq.com>
2024-08-21 08:44:58 +0800

    黑暗过渡添加动画

--- src/layout/components/Config/index.vue
+++ src/layout/components/Config/index.vue
@@ -43,6 +43,40 @@ const onCommitColorState = (value, name) => {
   }
   configStore.setLayout(name, colors)
 }
+const toggleDarkLight = (e) => {
+  const transition = document.startViewTransition(() => {
+    toggleDark()
+  })
+  transition.ready.then(() => {
+    // 由于我们要从鼠标点击的位置开始做动画，所以我们需要先获取到鼠标的位置
+    const { clientX, clientY } = e
+
+    // 计算半径，以鼠标点击的位置为圆心，到四个角的距离中最大的那个作为半径
+    const radius = Math.hypot(
+      Math.max(clientX, innerWidth - clientX),
+      Math.max(clientY, innerHeight - clientY)
+    )
+    const clipPath = [
+      `circle(0% at ${clientX}px ${clientY}px)`,
+      `circle(${radius}px at ${clientX}px ${clientY}px)`,
+    ]
+    const isDark = configStore.isDark
+    // 自定义动画
+    document.documentElement.animate(
+      {
+        // 如果要切换到暗色主题，我们在过渡的时候从半径 100% 的圆开始，到 0% 的圆结束
+        clipPath: isDark ? clipPath.reverse() : clipPath,
+      },
+      {
+        duration: 500,
+        // 如果要切换到暗色主题，我们应该裁剪 view-transition-old(root) 的内容
+        pseudoElement: isDark
+          ? '::view-transition-old(root)'
+          : '::view-transition-new(root)',
+      }
+    )
+  })
+}
 const restoreDefault = () => {
   Local.remove(STORE_CONFIG)
   Local.remove(BEFORE_RESIZE_LAYOUT)
@@ -144,7 +178,7 @@ const restoreDefault = () => {
             <el-divider border-style="dashed"> 全局 </el-divider>
             <div class="layout-config-global">
               <el-form-item size="large" label="暗黑模式">
-                <DarkSwitch @click="toggleDark()" />
+                <DarkSwitch @click="toggleDarkLight" />
               </el-form-item>
               <el-form-item label="后台页面切换动画">
                 <el-select
