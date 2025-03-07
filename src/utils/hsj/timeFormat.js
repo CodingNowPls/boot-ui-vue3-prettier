364410141c31d2333bfe6915668e4221db07bebe
coderlmw <2383250163@qq.com>
2025-03-07 08:06:01 +0800

    修复parseTime的bug

--- src/utils/hsj/timeFormat.js
+++ src/utils/hsj/timeFormat.js
@@ -3,5 +3,5 @@ import utc from 'dayjs/plugin/utc'
 dayjs.extend(utc)
 const HOUR_MINUTE_SECOND = 'YYYY-MM-DD HH:mm:ss'
 export function parseTime(utc, format = HOUR_MINUTE_SECOND) {
-  return dayjs.utc(utc).utcOffset(8).format(format)
+  return dayjs(utc).format(format)
 }
